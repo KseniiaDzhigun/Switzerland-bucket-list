8f029b24333bf93d02d4519b3f4dd06378473afd
Kseniia Dzhigun <kseniia.balandina@gmail.com>
2023-02-01 15:07:29 +0100

    fix: baseUrl from loclahost to domain name

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -103,8 +103,8 @@ class Api {
 }
 
 const api = new Api({
-  //baseUrl: 'https://api.dzhigun.nomoredomains.rocks',
-  baseUrl: 'http://localhost:3000',
+  baseUrl: 'https://api.dzhigun.nomoredomains.rocks',
+  // baseUrl: 'http://localhost:3000',
   headers: {
     'Content-Type': 'application/json'
   }
